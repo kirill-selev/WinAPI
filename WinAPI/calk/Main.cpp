fec7cf4f851e9314f0b4d5e59fa013f2f7c5bd41
ckaul <ckaul@KIRILL>
2024-08-22 14:10:20 +0700

    Done

---
+++ WinAPI/calk/Main.cpp
@@ -0,0 +1,134 @@
+#include <Windows.h>
+#include "resource.h" 
+
+
+BOOL CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lparam);
+CONST CHAR g_sz_WINDOWS_CLASS[] = "Calc_PD_311";
+
+
+
+CONST INT g_i_START_X = 10;
+CONST INT g_i_START_Y = 10;
+
+CONST INT g_i_BUTTON_SIZE = 50;
+CONST INT g_i_INTERVAL = 5;
+
+CONST INT g_i_DISPLAY_WIDTH = (g_i_BUTTON_SIZE + g_i_INTERVAL) * 5;
+CONST INT g_i_DISPLAY_HEIGHT = 22;
+
+CONST INT g_i_WINDOW_WIDTH = g_i_DISPLAY_WIDTH + g_i_START_X * 2 + 16;
+CONST INT g_i_WINDOW_HEIGHT = g_i_DISPLAY_HEIGHT + g_i_START_Y * 2 + (g_i_BUTTON_SIZE + g_i_INTERVAL) * 4;
+
+CONST INT g_i_START_X_BUTTON = g_i_START_X;
+CONST INT g_i_START_Y_BUTTON = g_i_START_Y * 2 + g_i_DISPLAY_HEIGHT;
+
+
+
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreveInst, LPSTR IpCmdLine, INT nCmdShow)
+{
+	//1)регистрация окна 
+	WNDCLASSEX wClass;
+	ZeroMemory(&wClass, sizeof(wClass));
+	wClass.style = 0;
+	wClass.cbSize = sizeof(wClass);
+	wClass.cbWndExtra = 0;
+	wClass.cbClsExtra = 0;
+
+	wClass.hIcon = (HICON)LoadImage(hInstance, "calc.ico", IMAGE_ICON, LR_DEFAULTSIZE, IMAGE_ICON, LR_DEFAULTSIZE);
+	wClass.hIconSm = (HICON)LoadImage(hInstance, "calc.ico", IMAGE_ICON, LR_DEFAULTSIZE, IMAGE_ICON, LR_DEFAULTSIZE);
+	wClass.hCursor = LoadCursor(hInstance, IDC_ARROW);
+	wClass.hbrBackground = (HBRUSH)COLOR_WINDOW;
+
+	wClass.hInstance = hInstance;
+	wClass.lpszClassName = g_sz_WINDOWS_CLASS;
+	wClass.lpfnWndProc = (WNDPROC)WndProc;
+	wClass.lpszMenuName = NULL;
+
+	if (!RegisterClassEx(&wClass))
+	{
+		MessageBox(NULL, "Class register failed", "Error", MB_OK | MB_ICONERROR);
+		return 0;
+	}
+
+
+	//2)window create
+
+	HWND hwnd = CreateWindowExA
+	(
+		NULL,
+		g_sz_WINDOWS_CLASS,
+		g_sz_WINDOWS_CLASS,
+		WS_OVERLAPPEDWINDOW^WS_THICKFRAME^WS_MAXIMIZEBOX,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		g_i_WINDOW_WIDTH, g_i_WINDOW_HEIGHT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+
+	);
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+
+
+	MSG msg;
+	while (GetMessage(&msg, hwnd, 0, 0) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+}
+INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	 case WM_CREATE:
+	 {
+
+		HWND hDisplay = CreateWindowEx
+		(
+			NULL, "Edit", "0",
+			WS_CHILD | WS_VISIBLE | WS_BORDER | ES_NUMBER | ES_RIGHT | ES_READONLY,
+			g_i_START_X, g_i_START_Y,
+			g_i_DISPLAY_WIDTH, g_i_DISPLAY_HEIGHT,
+			hwnd,
+			(HMENU)IDC_EDIT_DISPLAY,
+			NULL,
+			NULL
+		);
+
+		INT digit = 0;
+		CHAR sz_digit[2]{};
+		for (int i = 2; i >= 0; i--)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				sz_digit[0] = '1' + digit;
+				CreateWindowEx
+				(
+					NULL, "Button", sz_digit,
+					WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+					g_i_START_X_BUTTON + j * (g_i_BUTTON_SIZE + g_i_INTERVAL),
+					g_i_START_Y_BUTTON + i * (g_i_BUTTON_SIZE + g_i_INTERVAL),
+					g_i_BUTTON_SIZE, g_i_BUTTON_SIZE,
+					hwnd,
+					(HMENU)(IDC_BUTTON_1 + digit++),
+					NULL,
+					NULL
+				);
+			}
+		}
+
+	 }
+	case WM_COMMAND:
+		break;
+	case WM_DESTROY:PostQuitMessage(0);break;
+
+	case WM_CLOSE:DestroyWindow(hwnd);break;
+
+	default:return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	}
+	return 0;
+
+}
