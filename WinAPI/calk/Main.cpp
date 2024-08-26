1914449faff51be9f31ae6030c1cb6e25876289e
ckaul <ckaul@KIRILL>
2024-08-26 19:06:00 +0700

    Button DONE

--- WinAPI/calk/Main.cpp
+++ WinAPI/calk/Main.cpp
@@ -1,8 +1,10 @@
-#include <Windows.h>
-#include "resource.h" 
+#define _CRT_SECURE_NO_WARNINGS
+#include<Windows.h>
+#include"resource.h"
 
 
 BOOL CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lparam);
+VOID PushButton(HWND parent, INT id);
 CONST CHAR g_sz_WINDOWS_CLASS[] = "Calc_PD_311";
 
 
@@ -12,15 +14,19 @@ CONST INT g_i_START_Y = 10;
 
 CONST INT g_i_BUTTON_SIZE = 50;
 CONST INT g_i_INTERVAL = 5;
+CONST INT g_i_BUTTON_DOUBLE_SIZE = g_i_BUTTON_SIZE * 2 + g_i_INTERVAL;
 
 CONST INT g_i_DISPLAY_WIDTH = (g_i_BUTTON_SIZE + g_i_INTERVAL) * 5;
 CONST INT g_i_DISPLAY_HEIGHT = 22;
 
+CONST INT g_i_TITLE_HEIGHT = 39;
 CONST INT g_i_WINDOW_WIDTH = g_i_DISPLAY_WIDTH + g_i_START_X * 2 + 16;
-CONST INT g_i_WINDOW_HEIGHT = g_i_DISPLAY_HEIGHT + g_i_START_Y * 2 + (g_i_BUTTON_SIZE + g_i_INTERVAL) * 4;
+CONST INT g_i_WINDOW_HEIGHT = g_i_DISPLAY_HEIGHT + g_i_START_Y * 2 + (g_i_BUTTON_SIZE + g_i_INTERVAL) * 4 + g_i_TITLE_HEIGHT + 5;
 
 CONST INT g_i_START_X_BUTTON = g_i_START_X;
 CONST INT g_i_START_Y_BUTTON = g_i_START_Y * 2 + g_i_DISPLAY_HEIGHT;
+CONST INT g_i_START_X_OPERATIONS = g_i_START_X_BUTTON + (g_i_BUTTON_SIZE + g_i_INTERVAL) * 3;
+CONST INT g_i_START_X_CONTROL_BUTTONS = g_i_START_X_BUTTON + (g_i_BUTTON_SIZE + g_i_INTERVAL) * 4;
 
 
 
@@ -59,7 +65,7 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreveInst, LPSTR IpCmdLine, I
 		NULL,
 		g_sz_WINDOWS_CLASS,
 		g_sz_WINDOWS_CLASS,
-		WS_OVERLAPPEDWINDOW^WS_THICKFRAME^WS_MAXIMIZEBOX,
+		WS_OVERLAPPEDWINDOW ^ WS_THICKFRAME ^ WS_MAXIMIZEBOX,
 		CW_USEDEFAULT, CW_USEDEFAULT,
 		g_i_WINDOW_WIDTH, g_i_WINDOW_HEIGHT,
 		NULL,
@@ -83,8 +89,14 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	switch (uMsg)
 	{
-	 case WM_CREATE:
-	 {
+	case WM_CREATE:
+	{
+		RECT rectWindow;
+		RECT rectClient;
+		GetWindowRect(hwnd, &rectWindow);
+		GetClientRect(hwnd, &rectClient);
+		int window_heignt = rectWindow.bottom - rectWindow.top;
+		int title_heigt = window_heignt - rectClient.bottom;
 
 		HWND hDisplay = CreateWindowEx
 		(
@@ -92,12 +104,10 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			WS_CHILD | WS_VISIBLE | WS_BORDER | ES_NUMBER | ES_RIGHT | ES_READONLY,
 			g_i_START_X, g_i_START_Y,
 			g_i_DISPLAY_WIDTH, g_i_DISPLAY_HEIGHT,
-			hwnd,
-			(HMENU)IDC_EDIT_DISPLAY,
-			NULL,
-			NULL
+			hwnd, (HMENU)IDC_EDIT_DISPLAY,
+			NULL, NULL
 		);
-
+		//////////////////////digits:///////////////////
 		INT digit = 0;
 		CHAR sz_digit[2]{};
 		for (int i = 2; i >= 0; i--)
@@ -118,11 +128,239 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 					NULL
 				);
 			}
+
+			CreateWindowEx
+			(NULL, "Button", "0",
+				WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+				g_i_START_X_BUTTON, g_i_START_Y_BUTTON + (g_i_BUTTON_SIZE + g_i_INTERVAL) * 3,
+				g_i_BUTTON_DOUBLE_SIZE, g_i_BUTTON_SIZE,
+				hwnd,
+				(HMENU)IDC_BUTTON_0,
+				NULL,
+				NULL
+			);
+			CreateWindowEx
+			(
+
+				NULL, "Button", ".",
+				WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+
+				g_i_START_X_BUTTON + g_i_BUTTON_DOUBLE_SIZE + g_i_INTERVAL,
+				g_i_START_Y_BUTTON + (g_i_BUTTON_SIZE + g_i_INTERVAL) * 3,
+				g_i_BUTTON_SIZE, g_i_BUTTON_SIZE,
+
+				hwnd,
+				(HMENU)IDC_BUTTON_POINT,
+				NULL,
+				NULL
+
+			);
+			//////////////////////operations:///////////////////
+			CONST CHAR sz_operations[] = "+-*/";
+			CHAR sz_operation[2] = "";
+			for (int i = 0; i < 4; i++)
+			{
+				sz_operation[0] = sz_operations[i];
+				CreateWindowEx
+				(
+					NULL, "Button", sz_operation,
+					WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+
+					g_i_START_X_OPERATIONS, g_i_START_Y_BUTTON + (g_i_BUTTON_SIZE + g_i_INTERVAL) * (3 - i),
+					g_i_BUTTON_SIZE, g_i_BUTTON_SIZE,
+
+					hwnd,
+					(HMENU)(IDC_BUTTON_PLUS + i),
+					NULL,
+					NULL
+				);
+			}
+
+			CreateWindowEx
+			(
+				NULL, "Button", "<-",
+				WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+
+				g_i_START_X_CONTROL_BUTTONS, g_i_START_Y_BUTTON,
+				g_i_BUTTON_SIZE, g_i_BUTTON_SIZE,
+
+				hwnd,
+				(HMENU)IDC_BUTTON_BSP,
+				NULL,
+				NULL
+			);
+			CreateWindowEx
+			(
+				NULL, "Button", "C",
+				WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+
+				g_i_START_X_CONTROL_BUTTONS, g_i_START_Y_BUTTON + g_i_BUTTON_SIZE + g_i_INTERVAL,
+				g_i_BUTTON_SIZE, g_i_BUTTON_SIZE,
+
+				hwnd,
+				(HMENU)IDC_BUTTON_CLEAR,
+				NULL,
+				NULL
+			);
+
+			CreateWindowEx
+			(
+				NULL, "Button", "=",
+				WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+
+				g_i_START_X_CONTROL_BUTTONS, g_i_START_Y_BUTTON + g_i_BUTTON_DOUBLE_SIZE + g_i_INTERVAL,
+				g_i_BUTTON_SIZE, g_i_BUTTON_DOUBLE_SIZE,
+
+				hwnd,
+				(HMENU)IDC_BUTTON_EQUAL,
+				NULL,
+				NULL
+			);
+
+
+
 		}
 
-	 }
+	}
 	case WM_COMMAND:
-		break;
+	{
+		SetFocus(hwnd);
+		CONST INT SIZE = 256;
+		CHAR sz_display[SIZE]{};
+		CHAR sz_digit[2]{};
+		HWND hEditDisplay = GetDlgItem(hwnd, IDC_EDIT_DISPLAY);
+		if (LOWORD(wParam) >= IDC_BUTTON_0 && LOWORD(wParam) <= IDC_BUTTON_POINT)
+		{
+			sz_digit[0] = LOWORD(wParam) - IDC_BUTTON_0 + '0';
+			SendMessage(hEditDisplay, WM_GETTEXT, SIZE, (LPARAM)sz_display);
+			if (LOWORD(wParam) == IDC_BUTTON_POINT)
+			{
+				if (strchr(sz_display, '.'))break;
+				strcat(sz_display, ".");
+			}
+			else
+			{
+				if (sz_display[0] == '0' && strlen(sz_display) == 1)sz_display[0] = sz_digit[0];
+				else strcat(sz_display, sz_digit);
+			}
+			SendMessage(hEditDisplay, WM_SETTEXT, 0, (LPARAM)sz_display);
+		}
+		if (LOWORD(wParam) == IDC_BUTTON_BSP)
+		{
+			SendMessage(hEditDisplay, WM_GETTEXT, SIZE, (LPARAM)sz_display);
+			INT display_length = strlen(sz_display);
+			if (display_length)
+				sz_display[--display_length] = 0;
+			if (display_length == 0)sz_display[0] = '0';
+			SendMessage(hEditDisplay, WM_SETTEXT, 0, (LPARAM)sz_display);
+		}
+		if (LOWORD(wParam) == IDC_BUTTON_CLEAR)
+		{
+			SendMessage(hEditDisplay, WM_SETTEXT, 0, (LPARAM)"0");
+		}
+	}
+	break;
+	case WM_KEYUP:
+	{
+		if (GetKeyState(VK_SHIFT) < 0 && LOWORD(wParam) == 0x38)
+		{
+		    SendMessage(hwnd,WM_COMMAND,IDC_BUTTON_ASTER,0);
+			SendMessage(GetDlgItem(hwnd, IDC_BUTTON_ASTER), BM_SETSTATE, FALSE, 0);
+		}
+		
+		else if (LOWORD(wParam) >= 0x30 && LOWORD(wParam) <= 0x39)
+		{
+			SendMessage(hwnd, WM_COMMAND, LOWORD(wParam - 0x30 + IDC_BUTTON_0), 0);
+			SendMessage(GetDlgItem(hwnd, wParam - 0x30 + IDC_BUTTON_0), BM_SETSTATE, FALSE, 0);
+		}
+
+		else if (LOWORD(wParam) >= 0x60 && LOWORD(wParam) <= 0x69)
+		{
+			SendMessage(hwnd, WM_COMMAND, LOWORD(wParam - 0x60 + IDC_BUTTON_0), 0);
+			SendMessage(GetDlgItem(hwnd, wParam - 0x60 + IDC_BUTTON_0), BM_SETSTATE, FALSE, 0);
+		}
+
+		switch (LOWORD(wParam))
+		{
+		case VK_SEPARATOR:
+		case VK_OEM_PERIOD:
+			SendMessage(hwnd, WM_COMMAND, LOWORD(IDC_BUTTON_POINT), 0);
+			SendMessage(GetDlgItem(hwnd, IDC_BUTTON_POINT), BM_SETSTATE, FALSE, 0);
+			break;
+		case VK_OEM_PLUS:
+			SendMessage(hwnd, WM_COMMAND, LOWORD(IDC_BUTTON_PLUS), 0);
+			SendMessage(GetDlgItem(hwnd, IDC_BUTTON_PLUS), BM_SETSTATE, FALSE, 0);
+			break;
+		case VK_SUBTRACT:
+		case VK_OEM_MINUS:
+			SendMessage(hwnd, WM_COMMAND, LOWORD(IDC_BUTTON_MINUS), 0);
+			SendMessage(GetDlgItem(hwnd, IDC_BUTTON_MINUS), BM_SETSTATE, FALSE, 0);
+			break;
+		case VK_MULTIPLY:
+			SendMessage(hwnd, WM_COMMAND, LOWORD(IDC_BUTTON_ASTER), 0);
+			SendMessage(GetDlgItem(hwnd, IDC_BUTTON_ASTER), BM_SETSTATE, FALSE, 0);
+			break;
+		case VK_OEM_2:
+		case VK_DIVIDE:
+			SendMessage(hwnd, WM_COMMAND, LOWORD(IDC_BUTTON_SLASH), 0);
+			SendMessage(GetDlgItem(hwnd, IDC_BUTTON_SLASH), BM_SETSTATE, FALSE, 0);
+			break;
+
+		case VK_BACK:
+			SendMessage(hwnd, WM_COMMAND, LOWORD(IDC_BUTTON_BSP), 0);
+			SendMessage(GetDlgItem(hwnd, IDC_BUTTON_BSP), BM_SETSTATE, FALSE, 0);
+			break;
+		case VK_ESCAPE:
+			SendMessage(hwnd, WM_COMMAND, LOWORD(IDC_BUTTON_CLEAR), 0);
+			SendMessage(GetDlgItem(hwnd, IDC_BUTTON_CLEAR), BM_SETSTATE, FALSE, 0);
+			break;
+		case VK_RETURN:
+		  SendMessage(hwnd,WM_COMMAND,IDC_BUTTON_EQUAL,0);
+		  SendMessage(GetDlgItem(hwnd, IDC_BUTTON_EQUAL), BM_SETSTATE, FALSE, 0);
+		  break;
+		}
+
+	}
+	break;
+
+	case WM_KEYDOWN:
+	{
+		if (GetKeyState(VK_SHIFT) < 0 && LOWORD(wParam) == 0x38) 
+		{
+		 SendMessage(GetDlgItem(hwnd,IDC_BUTTON_ASTER),BM_SETSTATE,TRUE,0);
+		}
+		else if (LOWORD(wParam) >= 0x30 && LOWORD(wParam) <= 0x39)
+		{
+			SendMessage(hwnd, WM_COMMAND, LOWORD(wParam - 0x30 + IDC_BUTTON_0), 0);
+			SendMessage(GetDlgItem(hwnd, wParam - 0x30 + IDC_BUTTON_0), BM_SETSTATE, TRUE, 0);
+
+		}
+
+		else if (LOWORD(wParam) >= 0x60 && LOWORD(wParam) <= 0x69)
+		{
+			SendMessage(hwnd, WM_COMMAND, LOWORD(wParam - 0x60 + IDC_BUTTON_0), 0);
+			SendMessage(GetDlgItem(hwnd, wParam - 0x60 + IDC_BUTTON_0), BM_SETSTATE, TRUE, 0);
+		}
+
+		switch (LOWORD(wParam))
+		{
+		case VK_SEPARATOR:
+		case VK_OEM_PERIOD: SendMessage(GetDlgItem(hwnd, IDC_BUTTON_POINT), BM_SETSTATE, TRUE, 0); break;
+		case VK_OEM_PLUS:   SendMessage(GetDlgItem(hwnd, IDC_BUTTON_PLUS), BM_SETSTATE, TRUE, 0); break;
+		case VK_OEM_MINUS:  SendMessage(GetDlgItem(hwnd, IDC_BUTTON_MINUS), BM_SETSTATE, TRUE, 0); break;
+		case VK_SUBTRACT:
+		case VK_MULTIPLY:   SendMessage(GetDlgItem(hwnd, IDC_BUTTON_ASTER), BM_SETSTATE, TRUE, 0); break;
+		case VK_OEM_2:
+		case VK_DIVIDE:     SendMessage(GetDlgItem(hwnd, IDC_BUTTON_SLASH), BM_SETSTATE, TRUE, 0); break;
+
+		case VK_BACK:       SendMessage(GetDlgItem(hwnd, IDC_BUTTON_BSP), BM_SETSTATE, TRUE, 0); break;
+		case VK_ESCAPE:     SendMessage(GetDlgItem(hwnd, IDC_BUTTON_CLEAR), BM_SETSTATE, TRUE, 0); break;
+		case VK_RETURN : SendMessage(GetDlgItem(hwnd, IDC_BUTTON_EQUAL), BM_SETSTATE, TRUE, 0); break;
+		}
+
+	}
+	break;
+
 	case WM_DESTROY:PostQuitMessage(0);break;
 
 	case WM_CLOSE:DestroyWindow(hwnd);break;
@@ -130,5 +368,13 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	default:return DefWindowProc(hwnd, uMsg, wParam, lParam);
 	}
 	return 0;
+
+}
+
+VOID PushButton(HWND parent, INT id)
+{
+	SendMessage(GetDlgItem(parent, id), BM_SETSTATE, TRUE, 0);
+	Sleep(100);
+	SendMessage(GetDlgItem(parent, id), BM_SETSTATE, FALSE, 0);
 
 }
